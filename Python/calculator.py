c1f4b940983cb903d2560f6f2879e2e1635e27e7
Puja Singh <52859941+pujasingh1@users.noreply.github.com>
2021-03-17 12:24:46 -0700

    Update calculator.py
    Create a normal calculator for basic calculation of addition, subtraction, multiplication and division

--- Python/calculator.py
+++ Python/calculator.py
@@ -21,8 +21,7 @@ cal.title("Calculator")
 operator=""
 text_Input =StringVar()
 
-txtDisplay = Entry(cal,font=('arial', 20, 'bold'), textvariable=text_Input, bd=30, insertwidth=4,
-                   bg="powder blue", justify='right').grid(columnspan=4)
+txtDisplay = Entry(cal,font=('arial', 20, 'bold'), textvariable=text_Input, bd=30, insertwidth=4, bg="powder blue", justify='right').grid(columnspan=4)
 
 btn7=Button(cal,padx=16,pady=16,bd=8, fg="black",font=('arial', 20, 'bold'),text="7", bg="powder blue",command=lambda:btnClick(7)).grid(row=1,column=0)
 
