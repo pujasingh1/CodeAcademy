13587bf2550a866aec900a9252d9ef535b95f2f3
Puja Singh <52859941+pujasingh1@users.noreply.github.com>
2021-07-19 00:09:55 +0530

    Create Treasure_Island.py
    It's like a game, where we have to find out the hidden treasure behind 3 doors of a room.

---
+++ Python/Treasure_Island.py
@@ -0,0 +1,43 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+
+print("Welcome to the Treasure Island.")
+print("Your mission is to find the treasure.")
+
+choose = input('You\'re at a cross road. Where do you want to go? Type "left" or "right" \n').lower()
+
+if choose == "left":
+  choose1 = input('You come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. \n').lower()
+  if choose1 == "wait":
+    choose2 = input("You arrived at a island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which color do you choose? \n ").lower()
+ 
+    if choose2 == "red":
+      print("It\'s a room full of fire! Game Over")
+    elif choose2 == "yellow":
+      print("You found the treasure. You win!")
+    elif choose2 == "blue":
+      print("You entered a room of beast! Game Over")
+  else:
+    print("You get attacked by an angry trout. Game Over")
+else:
+  print("You fell into Hole. Game Over")
