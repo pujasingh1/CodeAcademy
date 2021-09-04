0856399ce5106e49a07089753317484aefd4677a
Puja Singh <52859941+pujasingh1@users.noreply.github.com>
2021-09-05 03:08:58 +0530

    Create Blind_auction.py

---
+++ Python/Blind_auction.py
@@ -0,0 +1,29 @@
+from replit import clear
+#HINT: You can call clear() to clear the output in the console.
+import art
+print(art.logo)
+print("Welcome to the secret auction program.")
+bids = {}
+should_continue = True
+
+def highest_bidder(bidding_record):
+  highest_bid = 0
+  winner = ""
+  # bidding_record = {"Puja": 123, "Rion": 321}
+  for bidder in bidding_record:
+    bid_amount = bidding_record[bidder]
+    if bid_amount > highest_bid:
+      highest_bid = bid_amount
+      winner = bidder
+  print(f"The winner is {winner} with the bid of ${highest_bid}")
+
+while should_continue:
+  name = input("What is your name?: ")
+  price = int(input("What's your bid?: $"))
+  bids[name] = price
+  ask_for_user = input("Are there any other bidders? Type 'yes' or 'no'.\n")
+  if ask_for_user == "yes":
+    clear()
+  elif ask_for_user == "no": 
+    should_continue = False
+    highest_bidder(bids)
