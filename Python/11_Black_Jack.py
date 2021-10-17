ef4f8262225244cf95208ade0b2ae35aa8d38fd8
Puja Singh <52859941+pujasingh1@users.noreply.github.com>
2021-10-18 00:55:33 +0530

    Update 11_Black_Jack.py

--- Python/11_Black_Jack.py
+++ Python/11_Black_Jack.py
@@ -1,26 +1,6 @@
-## Use the following list as the deck of cards:
+## The following list are the deck of cards:
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-## The cards in the list have equal probability of being drawn.
-## Cards are not removed from the deck as they are drawn.
-## The computer is the dealer.
 
-##################### Hints #####################
-
-#Hint 1: Go to this website and try out the Blackjack game: 
-#   https://games.washingtonpost.com/games/blackjack/
-#Then try out the completed Blackjack project here: 
-#   http://blackjack-final.appbrewery.repl.run
-
-#Hint 2: Read this breakdown of program requirements: 
-#   http://listmoz.com/view/6h34DJpvJBFVRlZfJvxF
-#Then try to create your own flowchart for the program.
-
-#Hint 3: Download and read this flow chart I've created: 
-#   https://drive.google.com/uc?export=download&id=1rDkiHCrhaf9eX7u7yjM1qwSuyEk-rPnt
-
-#Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
-#11 is the Ace.
-#cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 import random
 from replit import clear
 from art import logo
@@ -29,16 +9,11 @@ def deal_card():
   deal = random.choice(cards)
   return deal
 
-#Hint 6: Create a function called calculate_score() that takes a List of cards as input 
-#and returns the score. 
-#Look up the sum() function to help you do this.
 def calculate_score(cards):
   """Take a list of cards and return the score calculated from the cards"""
-  #Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
-  if sum(cards) == 21 and len(cards) == 2:
+    if sum(cards) == 21 and len(cards) == 2:
     return 0
-  #Hint 8: Inside calculate_score() check for an 11 (ace). If the score is already over 21, remove the 11 and replace it with a 1. You might need to look up append() and remove().
-  if 11 in cards and sum(cards) > 21:
+    if 11 in cards and sum(cards) > 21:
     cards.remove(11)
     cards.append(1)
   return sum(cards)
